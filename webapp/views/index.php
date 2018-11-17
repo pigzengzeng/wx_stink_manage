d5aaac710ae67ccc54bccc6b43ab586c3b0aab1a
zhouyuzeng <zhouyuzeng@makenv.com>
2018-11-17 10:31:40 +0800

    gitignore配置错误push文件未包含

---
+++ webapp/views/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>恶臭小程序管理后台</title>
+<?php require_once('global_css.php')?>
+</head>
+<body>
+
+<?php require_once('top_header.php')?>
+<?php require_once('sidebar_menu.php')?>
+
+<!--main-container-part-->
+<div id="content">
+<!--breadcrumbs-->
+  <div id="content-header">
+    <div id="breadcrumb"> <a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> 首页 面包屑 以后这里可以放dashboard</a></div>
+  </div>
+<!--End-breadcrumbs-->
+
+</div>
+<!--end-main-container-part-->
+
+<?php require_once('footer.php')?>
+</body>
+</html>
